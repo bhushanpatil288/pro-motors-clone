acb8599548d95663f34aa5fea823c33e959fcc71
BhushanPatil2882001 <sharewithbhushan@gmail.com>
2025-04-23 14:24:41 +0530

    add: srvices section > heading

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// services section
+let secondaryButtons = document.querySelectorAll(".sec-btn");
+
+secondaryButtons.forEach(button =>{
+    button.addEventListener("click", ()=>{
+        secondaryButtons.forEach(b=>{
+            b.classList.remove("active");
+        })
+        button.classList.add("active");
+    })
+})
