e03ccf1ff0bcbde588ca3550202b941e171d26c2
BhushanPatil2882001 <sharewithbhushan@gmail.com>
2025-04-28 14:25:21 +0530

    add: custom cursor

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,43 @@ secondaryButtons.forEach(button =>{
     })
 })
 
+// custom cursor start
+const cursor = document.getElementById('cursor');
+
+let mouseX = 0;
+let mouseY = 0;
+
+let cursorX = 0;
+let cursorY = 0;
+
+document.addEventListener('mousemove', (e) => {
+  mouseX = e.clientX + window.scrollX;
+  mouseY = e.clientY + window.scrollY;
+});
+
+function animateCursor() {
+  cursorX += (mouseX - cursorX) * 0.1;
+  cursorY += (mouseY - cursorY) * 0.1;
+
+  cursor.style.left = `${cursorX}px`;
+  cursor.style.top = `${cursorY}px`;
+
+  requestAnimationFrame(animateCursor);
+}
+
+animateCursor(); 
+
+document.addEventListener('mousedown', () => {
+  cursor.classList.add('clicked');
+});
+
+document.addEventListener('mouseup', () => {
+  cursor.classList.remove('clicked');
+});
+
+
+// custom cursor end
+
 // preloader
 
 let preloader = document.querySelector(".preloader");
